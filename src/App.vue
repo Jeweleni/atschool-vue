9d167fdeb9c32f7dc932c97cbef3da949dc4aea4
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2022-12-06 23:00:17 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AltschoolStudent/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import AltschoolStudent from './components/AtschoolStudent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    AltschoolStudent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      Hello Ikenna, nice to meet you
+    </p>
+    <p>{{count}}</p>
+    <button @click="incrementCount">Increment</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+      count: 12 ,
+    };
+  },
+
+
+
+methods:{
+  incrementCount() {
+    this.count++;
+  },
+},
+
+beforeCreate(){
+  console.log("beforeCreate has been calledd")
+},
+
+created(){
+  console.log("created has been called")
+},
+
+beforeMount(){
+  console.log("beforeMount has been called")
+},
+
+mounted(){
+  console.log("mounted has been called")
+},
+
+beforeUpdate(){
+  console.log("beforeUpdate has been called")
+},
+
+updated(){
+  console.log("update has been called")
+},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
