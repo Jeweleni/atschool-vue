7da47afcb2ecbdd92a390eb90ef83a1332c47dce
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2022-12-11 19:20:38 +0100

    components

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import AltschoolStudent from './components/AtschoolStudent.vue';
+import AltschoolStudent from './components/AltschoolStudent.vue';
 
 export default {
   name: 'App',
